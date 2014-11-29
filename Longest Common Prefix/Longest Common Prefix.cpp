8d36e94d2c8e691838ec73deac2d934247fd6d0e
zhoufei23 <zhoufei2324@gmail.com>
2014-11-29 22:51:49 +0800

    Add the inrger to roman and longest common prefix

---
+++ Longest Common Prefix/Longest Common Prefix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string> &strs) {
+        string result;
+        int i,j;
+        int flag =1;
+        if(strs.size() == 0)
+           return "";
+        for(i=0;i<strs[0].size();i++){
+            flag =1;
+            for(j=1;j<strs.size();j++){
+                if (strs[j][i] != strs[0][i]){
+                     flag =0;
+                     return result;
+                }
+            }
+            if(flag)
+                result+=strs[0][i];
+        }
+        return strs[0];
+    }
+};
