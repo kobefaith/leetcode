d1f0f73b38adf3ae2b13dc8bd010d03984e9a552
zhoufei23 <zhoufei2324@gmail.com>
2014-11-30 22:22:46 +0800

    add the add two numbers

---
+++ Add Two Numbers/Add Two Numbers .cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+              ListNode result(-1) ;
+              ListNode *first = &result;
+              int carry=0;
+              if(l1 == NULL && l2 == NULL)
+                  return NULL;
+              while(l1 != NULL || l2 != NULL){
+                  int value;
+                  if (l1 != NULL && l2 != NULL){
+                      value = l1->val+l2->val+carry;
+                      carry = value/10;
+                      value %= 10;
+                      l1 = l1->next;
+                      l2 = l2->next;
+                      first->next = new ListNode(value);
+                      first = first->next;
+                  }else if(l1 == NULL){
+                      value =l2->val +carry;
+                      carry = value/10;
+                      value %= 10;
+                      l2 = l2->next;
+                      first->next = new ListNode(value);
+                      first = first->next;
+                  }else{
+                     value =l1->val +carry;
+                      carry = value/10;
+                      value %= 10;
+                      l1 = l1->next;
+                      first->next = new ListNode(value);
+                      first = first->next;
+                  }
+              }
+              if(carry !=0)
+                  first->next = new ListNode(carry);
+              
+              return result.next;
+    }
+};
---
+++ Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int  findMedian(int A[], int m, int B[], int n,int k)  {
+        int index_a,index_b;
+        if(m >n) return findMedian(B,  n, A, m, k);
+        if(m == 0 )
+           return B[k-1];
+        if(k == 1)
+           return (A[0] <B[0])?A[0]:B[0];
+        index_a= (k/2 < m)? k/2 : m;
+        index_b=k-index_a;
+       if(A[index_a -1]< B[index_b -1]){
+            findMedian(A+index_a, m-index_a, B, n,k-index_a);
+        }else if(A[index_a -1]>B[index_b -1]){
+            findMedian(A, m, B+index_b,  n-index_b, k-index_b);
+        }else
+            return A[index_a -1];
+    }
+    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+        
+        if((m+n)%2 == 0){
+            return (findMedian(A,m,B,n,(m+n)/2)+findMedian(A,m,B,n,(m+n)/2+1))/2.0;
+        }else{ 
+             return findMedian(A,m,B,n,(m+n)/2+1);
+        }
+        
+    }
+};
