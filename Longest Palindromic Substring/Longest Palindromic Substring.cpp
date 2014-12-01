e0fd260c4aae77a3f4555e881f6d2ffcaf5dd06f
zhoufei23 <zhoufei2324@gmail.com>
2014-12-01 23:39:01 +0800

    Add the longest palindromic substring and longest substring without repeating characters

---
+++ Longest Palindromic Substring/Longest Palindromic Substring.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string temp;
+        string result;
+        int i=0;
+        int j;
+        int max_len=1;
+        int start,end;
+        if(s.size()==1)
+           return s;
+        while(i<s.size()){
+            temp += s[i];
+            temp += '*';
+            i++;
+        }
+        for(i=1;i<temp.size();i++){
+            j=1;
+            while(j<=i){
+                if(temp[i-j] != temp[i+j])
+                    break;
+                j++;
+            }
+            j-=1;
+            if(2*j+1 >max_len){
+                max_len = 2*j+1;
+                start = i-j;
+                end = i+j;
+            }
+        }
+        for(i=start;i<=end;i++){
+            if(temp[i] != '*')
+               result += temp[i];
+        }
+       return result; 
+    }
+};
---
+++ Longest Substring Without Repeating Characters/Longest Substring Without Repeating Characters.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int index[96];
+        int start=0,max_len=0;
+        int i=0;
+        if(s.size() == 0)
+           return 0;
+        for(i=0;i<96;i++)
+			index[i]=-1;
+		i=0;		
+        while(i<s.size()){
+                if(index[s[i]-' '] == -1){
+                    index[s[i]-' ']=i;
+                    i++;
+                }else {
+                   if((i-start) >max_len)
+                       max_len = i-start;
+                   if(start <(index[s[i]-' ']+1))
+						start = index[s[i]-' ']+1;
+                   index[s[i]-' ']= i;
+                   i++;
+                }
+        }
+        if((i-start) >max_len)
+           max_len = i-start;
+        return max_len;
+    }
+};
