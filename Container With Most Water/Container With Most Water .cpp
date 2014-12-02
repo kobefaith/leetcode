f939084f6645e59b77c095ac544a594c462fa082
zhoufei23 <zhoufei2324@gmail.com>
2014-12-02 23:03:25 +0800

    Add the container with most water and remove Nth node from end of list

---
+++ Container With Most Water/Container With Most Water .cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        int result=0;
+        int i=0,j=height.size()-1;
+        while(i<j){
+            if((height[i]>height[j]?height[j]:height[i])*(j-i)>result)
+                 result = (height[i]>height[j]?height[j]:height[i])*(j-i);
+            if(height[i]>height[j])
+               j--;
+            else
+                i++;
+        }
+        return result;
+    }
+};
---
+++ Remove Nth Node From End of List/Remove Nth Node From End of List.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n) {
+        ListNode *start=head;
+        ListNode *end=head;
+        int i=0;
+        while(end !=NULL&& i<n){
+            end = end->next;
+            i++;
+        }
+        if(end == NULL){
+            head = head->next;
+            return head;
+        }
+        while(end->next !=NULL){
+            start = start->next;
+            end =end->next;
+        }
+        start->next = start->next->next;
+        return head;
+        
+    }
+};
