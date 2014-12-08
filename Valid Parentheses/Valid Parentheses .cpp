66b7b8c1dc2e6fcbe4c5e64878534a7847f3c46e
zhoufei23 <zhoufei2324@gmail.com>
2014-12-08 22:05:11 +0800

    Add the Valid Parentheses

---
+++ Valid Parentheses/Valid Parentheses .cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> stk;
+        int i=0;
+        while(i<s.size()){
+            if(s[i] == '{'||s[i] == '('||s[i] == '['){
+                stk.push(s[i]);
+                i++;
+            }else if(s[i] == '}'||s[i] == ')'||s[i] == ']'){
+                if(stk.empty())
+					return false;
+                if((s[i] == '}'&& stk.top() == '{')||(s[i] == ')'&& stk.top() == '(')||(s[i] == ']'&& stk.top() == '[')){
+                   stk.pop();
+                   i++;
+                }else 
+                    return false;
+            }else
+                i++;
+            
+        }
+        if(stk.empty())
+            return true;
+        else
+            return false;
+    }
+};
