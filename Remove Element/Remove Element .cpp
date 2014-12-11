f1db7243a920d3ebdcd778477db18afbb5c42367
zhoufei23 <zhoufei2324@gmail.com>
2014-12-11 23:53:36 +0800

    Remove Element

---
+++ Remove Element/Remove Element .cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int removeElement(int A[], int n, int elem) {
+       int i = 0;
+        int j = n-1; 
+        if(n == 1)
+            return A[0] == elem ?0:1;
+        while(i<j){
+            while(A[i] != elem && i<n)
+                 i++;
+            while(A[j] == elem && j>=0)
+                 j--;
+			if(i<j){
+				A[i]=A[i]^A[j];
+				A[j]=A[i]^A[j];
+				A[i]=A[i]^A[j];				
+			}						
+        }
+        return j+1;
+    }
+};
