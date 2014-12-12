33fb8c43bceebfda0f39be59bfa6378fa46ce1eb
zhoufei23 <zhoufei2324@gmail.com>
2014-12-12 22:32:03 +0800

    Add Implement strStr()

---
+++ Implement strStr()/Implement strStr().cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(char *haystack, char *needle) {
+     int i=0;
+	 int j=0;
+	 if(strlen(needle)== 0)
+		 return 0;
+	 while(haystack[i] != '\0'){
+		 if(haystack[i] == needle[j]){
+			   i++;
+			   j++;
+		 }else{
+			 i=i-j+1;
+			 j=0;
+		 }
+		 if( needle[j] =='\0' )
+			 return i-j;           
+	 }	 
+	 return -1;
+ }
+};
