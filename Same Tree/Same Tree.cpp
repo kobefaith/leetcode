290bef1f5a805ef419bf44115645545c4806c0b9
zhoufei23 <zhoufei2324@gmail.com>
2014-12-20 23:15:28 +0800

    Add the Same Tree

---
+++ Same Tree/Same Tree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode *p, TreeNode *q) {
+        if(p == NULL && q != NULL)
+			return false;
+		else if(q== NULL && p != NULL)
+			return false;
+		else if(q== NULL && p == NULL)
+			return	true;
+		if(p->val != q->val)
+			return false;
+		else if(isSameTree(p->left,q->left) == false)
+			return false;
+		else if(isSameTree(p->right,q->right) == false)
+			return false;
+         return true;      
+    }
+};
---
+++ Valid Palindrome/Valid Palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+       string temp;
+       int i = 0,j;
+       if(s.size() == 0)
+	      return true;
+       for(i;i<s.size();i++){
+	      if((s[i] >= 'a'&& s[i] <= 'z')||(s[i] >= '0'&& s[i] <= '9'))
+		      temp += s[i];
+	      else if(s[i] >= 'A'&& s[i] <= 'Z'){
+		      temp += s[i]+32;
+	      }
+       }   
+       i = 0;
+       j = temp.size()-1;
+       while(i<j){
+	      if(temp[i] != temp[j])
+		      return false;
+	      i++;
+	      j--;
+       }
+      return true; 
+    }
+};
