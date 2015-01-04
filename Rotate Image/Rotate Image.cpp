c8d23b967abbdc2fc43ea1f7869c402e7e1a348a
zhoufei23 <zhoufei2324@gmail.com>
2015-01-04 22:17:51 +0800

    Add Rotate Image

---
+++ Rotate Image/Rotate Image.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    void rotate(vector<vector<int> > &matrix) {
+	 int temp;
+	 for(int i=0;i<matrix.size()/2;i++){
+		 for(int j=i;j<matrix[0].size()-i-1;j++){
+			 if(i != j){
+				 temp = matrix[i][j];
+				 matrix[i][j] = matrix[matrix.size()-1-j][i];
+				 matrix[matrix.size()-1-j][i] = temp;
+
+				 temp = matrix[matrix.size()-1-j][i];
+				 matrix[matrix.size()-1-j][i] = matrix[matrix.size()-1-i][matrix.size()-1-j];
+				 matrix[matrix.size()-1-i][matrix.size()-1-j] = temp;
+
+				 temp = matrix[matrix.size()-1-i][matrix.size()-1-j];
+				 matrix[matrix.size()-1-i][matrix.size()-1-j] = matrix[j][matrix.size()-1-i];
+				 matrix[j][matrix.size()-1-i] = temp;
+			 }else{
+				 temp = matrix[i][j];
+				 matrix[i][j] = matrix[matrix.size()-1-i][j];
+				 matrix[matrix.size()-1-i][j] = temp;
+
+				 temp = matrix[matrix.size()-1-i][j];
+				 matrix[matrix.size()-1-i][j] = matrix[matrix.size()-1-i][matrix.size()-1-j];
+				 matrix[matrix.size()-1-i][matrix.size()-1-j] = temp;
+
+				 temp = matrix[matrix.size()-1-i][matrix.size()-1-j];
+				 matrix[matrix.size()-1-i][matrix.size()-1-j] = matrix[i][matrix.size()-1-j];
+				 matrix[i][matrix.size()-1-j] = temp;
+			 }
+		 }
+	 }
+
+ }
+};
