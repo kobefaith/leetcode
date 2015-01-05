c0db4ffe9db12dd11bcb531ced46c17bc2853061
zhoufei23 <zhoufei2324@gmail.com>
2015-01-05 19:50:06 +0800

    Add Path Sum

---
+++ Path Sum/Path Sum .cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasPathSum(TreeNode *root, int sum) {
+        if(root == NULL )
+           return false;
+        if(root->left == NULL && root->right == NULL )
+            return sum == root->val;
+        if(hasPathSum(root->left,sum-root->val))
+			return true;	
+	    if(hasPathSum(root->right,sum-root->val))
+			return true;
+    }
+};
