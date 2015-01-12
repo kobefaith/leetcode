2bfa835f5a381f52157894123df07b7b469adad2
zhoufei23 <zhoufei2324@gmail.com>
2015-01-13 00:28:12 +0800

    Add Permutations II

---
+++ Permutations II/Permutations II.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<vector<int> > permuteUnique(vector<int> &num) {
+        vector<vector<int> > result;
+        sort(num.begin(), num.end());
+        do {
+           result.push_back(num);
+        } while(next_permutation(num.begin(), num.end()));
+        return result;
+    }
+};
