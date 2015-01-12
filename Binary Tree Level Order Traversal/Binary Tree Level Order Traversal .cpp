ed9281687ff80fcd1bbea13d243360ebccf269ac
zhoufei23 <zhoufei2324@gmail.com>
2015-01-13 00:22:49 +0800

    Add Binary Tree Level Order Traversal

--- Binary Tree Level Order Traversal/Binary Tree Level Order Traversal .cpp
+++ Binary Tree Level Order Traversal/Binary Tree Level Order Traversal .cpp
@@ -10,6 +10,34 @@
 class Solution {
 public:
     vector<vector<int> > levelOrder(TreeNode *root) {
-        
+       vector<vector<int> > result;
+	   vector<int> temp;
+	   queue<TreeNode*> qu;
+       TreeNode *pre = root;
+	   TreeNode *index ;
+	   TreeNode *cur ;
+	   
+	   if(root == NULL)
+		   return result;
+	   qu.push(root);
+	   while(!qu.empty()){
+	       do{
+	           index = qu.front();
+	           qu.pop();
+			   temp.push_back(index->val);
+			   if(index->left != NULL){
+			   	   qu.push(index->left);
+			   	   cur = index->left;
+			   }
+			   if(index->right != NULL){
+               	   qu.push(index->right);
+               	   cur = index->right;
+			   }
+		   }while(index != pre);
+	       result.push_back(temp);
+		   temp.clear();
+		   pre = cur;		
+	  }
+      return result; 
     }
 };
