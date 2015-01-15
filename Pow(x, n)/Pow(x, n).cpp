dcec852ed1ef7c4a685d47515fd712326f90cbd8
zhoufei23 <zhoufei2324@gmail.com>
2015-01-15 20:46:52 +0800

    Add Pow(x, n)

---
+++ Pow(x, n)/Pow(x, n).cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double pow(double x, int n) {
+        double result = 1.0;
+        int flag = 0;
+        if(n < 0){
+            flag = 1;
+            n *= -1;
+        }else if(n == 0)
+           return 1;
+        else if(n == 1)
+            return x;
+        if(n%2 == 1){
+           result = x;
+           n--;
+        }
+        if(flag == 0)
+           return result*pow(x*x,n/2);
+        else 
+           return 1.0/(result*pow(x*x,n/2));
+    }
+};
