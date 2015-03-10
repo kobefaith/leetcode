6fe95971fbd00c5967a7a53a140a0af570235f31
zhoufei23 <zhoufei2324@gmail.com>
2015-03-10 23:53:32 +0800

    Add Sort Colors

---
+++ Sort Colors/Sort Colors .cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void sortColors(int A[], int n) {
+        int red=0;
+        int white=0;
+        int blue=0;
+        int i;
+        for(i=0;i<n;i++){
+            if(A[i]==0)
+               red++;
+            else if(A[i]==1)
+               white++;
+            else
+               blue++;
+        }
+        i=0;
+        while(i<n){
+            if(i<red)
+               A[i]=0;
+            else if(i>=red && i<red+white)
+               A[i]=1;
+            else
+               A[i]=2;
+            i++;
+        }
+    }
+};
